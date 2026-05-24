79f9955ef1e5bb1fc7793daf226e402ed5615123
ssuchichan <ssuchichan@gmail.com>
2026-05-24 16:38:11 +0800

    feat: customize response

--- p01-hello/main.py
+++ p01-hello/main.py
@@ -70,8 +70,19 @@ async def get_file():
     return FileResponse(path)
 
 
+class News(BaseModel):
+    id: int
+    title: str
+    content: str
 
 
+@app.get("/news/{id}", response_model=News)
+async def get_news(id: int):
+    return {
+        "id": id,
+        "title": f"这是第{id}本书",
+        "content": "这是一本好书"
+    }
 
 
 
